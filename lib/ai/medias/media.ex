409cd82138cc09d450ea45ed5f686f2fc37bcf15
0xADADA <0xadada.pub@0xadada.pub>
2018-03-28 06:51:26 -0400

    fixed test

--- lib/ai/medias/media.ex
+++ lib/ai/medias/media.ex
@@ -30,12 +30,16 @@ defmodule Ai.Medias.Media do
     |> validate_required([:title, :url])
   end
 
-  def extract_provider_uid(changeset) do
+  defp extract_provider_uid(changeset) do
     url = Ecto.Changeset.get_field(changeset, :url)
-    provider_uid =
-      ~r{^.*(?:youtu\.be/|\w+/|v=)(?<id>[^#&?]*)}
-      |> Regex.named_captures(url)
-      |> get_in(["id"])
+    provider_uid = get_provider_uid(url)
     Ecto.Changeset.put_change(changeset, :provider_uid, provider_uid)
+  end
+
+  defp get_provider_uid(nil), do: nil
+  defp get_provider_uid(url) do
+    ~r{^.*(?:youtu\.be/|\w+/|v=)(?<id>[^#&?]*)}
+    |> Regex.named_captures(url)
+    |> get_in(["id"])
   end
 end
